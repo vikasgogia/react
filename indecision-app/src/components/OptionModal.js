2fb14ddfc710a1713c658d9ad311d1f34a37a62b
ut_vikas <vikasgog10@gmail.com>
2023-10-30 18:05:47 -0400

    mobile and modal style

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -4,13 +4,15 @@ import Modal from "react-modal"
 const OptionModal = (props) => (
   <Modal
     isOpen={!!props.selectedOption}
-    contentLabel="SelectedOption"
+    contentLabel="Selected Option"
     onRequestClose={props.handleCloseModal}
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleCloseModal}>okay</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleCloseModal}>okay</button>
   </Modal>
-)
+);
 
 export default OptionModal
